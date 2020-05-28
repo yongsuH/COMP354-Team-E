f74c58f36bc5d771d331aad304a74be7cb352ee2
am-isse <amina.isse@outlook.com>
2020-05-28 00:40:47 -0400

    Optimized the function

--- Functions/Power_Function.py
+++ Functions/Power_Function.py
@@ -1,7 +1,29 @@
-#Calculate the specified power of a specified base
+#Goal: Calculate the specified power of a specified base
+#Method: Exponentiation by squaring 
 
 def power_function (base, power):
-    result = 1
-    for counter in range(power): 
-        result *= base 
-    return result
+    #Case 1: Power is a negative number
+    if (power<0):
+        base = 1/base
+        power = -power
+
+    #Case 2: Power is equal to 0
+    if (power==0):
+        return 1
+    
+    #Case 3: Power is equal to 1
+    if (power==1):
+        return base
+    
+    #Case 4: Power is a positive number greater than 1
+    result=1
+    while(power>1):
+        if(power%2==0):
+            base=(base*base)
+            power=power/2
+        else: 
+            result=base*result
+            base=base*base
+            power=(power-1)/2
+    
+    return base*result
