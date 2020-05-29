93bfd291a614db12b0fc3a36f13e8070756dd907
seanheinrichs <seanheinrichs@gmail.com>
2020-05-29 08:38:04 -0400

    use function definition for length

--- Functions/Mean_Absolute_Deviation_Function.py
+++ Functions/Mean_Absolute_Deviation_Function.py
@@ -4,39 +4,32 @@
 # 3. Divide the sum by the number of values
 
 # Primary Algoirthm
-# -----------------
-def mean_absolute_deviation(input):
+def mean_absolute_deviation(args):
+    
     # Local variables
-    mean = calculate_mean(input)
+    mean = calculate_mean(args)
     dividend = 0
-    diviser = calculate_length(input)
+    diviser = len(args)
 
     # Calculate the dividend
-    for x in input:
+    for x in args:
         dividend += absolute_value(x - mean)
 
     # Return the Mean Absolute Deviation
     return (float(dividend) / diviser)
         
 # Helper Functions
-# ----------------
 def calculate_mean(input): 
     sum = 0
     for x in input:
         sum += x
-    return (sum / calculate_length(input))
+    return (sum / len(input))
 
 def absolute_value(input):
     if input < 0:
         return -input
     else:
         return input
-
-def calculate_length(input):
-    n = 0
-    for x in input:
-        n = n + 1
-    return n
 
 # Test Functions
 # --------------
@@ -47,5 +40,10 @@ x = mean_absolute_deviation([600, 470, 170, 430, 300])
 # Should equal 3.75
 y = mean_absolute_deviation([3, 6, 6, 7, 8, 11, 15, 16])
 
+# Should equal 0.0
+z = mean_absolute_deviation([0, 0, 0, 0, 0, 0])
+
+
 print(x)
 print(y)
+print(z)
